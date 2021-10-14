113f675fd3c8815730346a14f463d53048b74fe6
PaulMouchel <paulmouchel@live.fr>
2021-10-14 21:20:17 +0400

    add next.config.js in tracked files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+  module.exports = {
+    env: {
+      CONTENTFUL_SPACE_ID:process.env.CONTENTFUL_SPACE_ID,
+      CONTENTFUL_ACCESS_TOCKEN:process.env.CONTENTFUL_ACCESS_TOCKEN
+    },
+    images: {
+      domains: ['images.ctfassets.net'],
+    },
+  }
