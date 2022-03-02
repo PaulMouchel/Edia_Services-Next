828a7334a2138e27cd7b573fb9135948edc912fc
PaulMouchel <paulmouchel@live.fr>
2022-03-02 12:12:38 +0400

    fix responsive issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       'xxxs': '360px',
       'xxs': '420px',
       'xs': '560px',
+      'sm':	'640px',
+      'md':	'768px',
+      'lg':	'1024px', 
+      'xl':	'1280px',
+      '2xl': '1536px',
     },
     colors: {
       darkBlue: '#273F7F',
